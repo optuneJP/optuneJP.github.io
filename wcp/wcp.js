1171fd533c79dff11f0f6defcffd3b339373955c
optuneJP <78325952+optuneJP@users.noreply.github.com>
2024-04-23 16:55:39 +0900

    Update wcp.js

--- wcp/wcp.js
+++ wcp/wcp.js
@@ -4976,6 +4976,7 @@ function cameraAcquisition() {
         devices.forEach((device) => {
             if (device.kind === 'videoinput') {
                 if (
+                    device.label === 'USB Camera L-835 (04f2:1601)' ||
                     device.label === 'USB Camera L-836 (04f2:1601)' ||
                     device.label === 'USB Camera L-837 (04f2:1601)' ||
                     device.label === 'USB Camera L-834 (04f2:1601)'
